4a166d5426c4c47e53ec6df4baeade5f26b5677e
Mehamuda <mehamuda18.27@gmail.com>
2026-09-22 16:08:23 +0530

    Add files via upload

---
+++ Ex3.java
@@ -0,0 +1,21 @@
+class YourThread extends Thread{
+	public void run(){
+		System.out.println("Thread Name: "+Thread.currentThread().getName());
+		System.out.println("Thread Name: "+Thread.currentThread().getPriority());
+		for (int i=1; i<6; i++){
+			System.out.println(i);
+		}
+	}
+}
+class Ex{
+	public static void main (String args[]){
+		System.out.println("Thread Name: "+Thread.currentThread().getName());
+		System.out.println("Thread Name: "+Thread.currentThread().getPriority());
+		YourThread t1=new YourThread();
+		t1.start();
+		t1.setName("CSMC");
+		t1.setPriority(8);
+		YourThread t2=new YourThread();
+		t2.start();
+	}
+}
